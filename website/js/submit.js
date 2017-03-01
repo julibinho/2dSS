d85bfe4817d5f269c45df90dc83a0d8adbf7ab8e
Priya <dikshlot@gmail.com>
2017-03-01 13:22:42 +0100

    format contenu boite1

--- website/js/submit.js
+++ website/js/submit.js
@@ -2,103 +2,196 @@ var validation = document.getElementById('submit_button');
 validation.addEventListener('click',f_valid);
 
 var champ_predvide = document.getElementById("predvide");
+var champ_alignvide = document.getElementById("alignvide");
+
+//Var global stockant les noms eg d1x9fc_, d1jl7a_ ...
+var arraySeq = []; 
 
 
 //Fonction principale de validation quand on click sur le bouton submit
 function f_valid(e){
-   // var cm = content_missing();
-    //var fa= format_align();
-    //alert("le or");
-   // alert( ( cm|| fa));
-    if(content_missing() ){
-		//|| format_align()){
-      //  alert("content missing or format align");
-      //  alert(content_missing());
-       // alert(format_align());
-       // alert("Well a content is missing")
+    if(content_missing_box_align() ){
+                e.preventDefault();
+    }
+    if (content_missing_box_pred()) {
         e.preventDefault();
     }
+    
+    if (format_incorrect_box_align()){
+        e.preventDefault();
+        
+    }
+    
+    if (format_incorrect_box_pred()){
+        e.preventDefault();
+    }
+ }
+ 
+// Fonction testant si al boite 1 est vide  
+function content_missing_box_align(){
+        var align = document.getElementById("align").value;
+        if(align.length < 1 ){
+                   //alert('alignlength <1');
+                  
+            champ_alignvide.textContent = "Contents missing";
+            champ_alignvide.style.color = "red";
+            return true;
+        }
+        
+        champ_alignvide.textContent="";
+        return false;  
 }
 
-//Fonction testant si le contenus des cases est vide
-//renvoie true si une des cases est vide
-//et affiche le message "contents missing" a cote de la case vide
-function content_missing(){
-    //alert("passe dans fonc content missing()");
-   var align = document.getElementById("align").value;
-   var pred = document.getElementById("pred").value;
-   var bool=false;
-   
-    
-     if(align.length < 1 ){
-                   //alert('alignlength <1');
-                   var champ_alignvide = document.getElementById("alignvide");
-                   champ_alignvide.textContent = "Contents missing";
-                   champ_alignvide.style.color = "red";
-                    bool = true;
-     }             
-                    
-    if(pred.length < 1 ){
+// Fonction testant si la boite 2 est vide
+function content_missing_box_pred(){ 
+        var pred = document.getElementById("pred").value;
+        if(pred.length < 1 ){
                     //alert('pred length <1');
                     // var champ_predvide = document.getElementById("predvide");
-                     champ_predvide.textContent = "Contents missing";
-                     champ_predvide.style.color = "red";
-                     bool = true;
-            
-    }
-   // alert("content missing retourne");
-    //alert(bool);
-    return bool;
+            champ_predvide.textContent = "Contents missing";
+            champ_predvide.style.color = "red";
+            return true;
+        }
+        
+        champ_predvide.textContent = "";
+        return false;
+              
 }
 
-//Fonction verifiant le format du texte align
-// renvoie true si le format est incorrecte
-// NOTE : MAUVAIS TEST POUR LE MOMENT JUSTE EN TRAIN DE TESTER LONG = 3 
-function format_align(){
+// Fonction testant si le format dans boite 1 incorrecte
+function format_incorrect_box_align(){
     
-      if(content_missing()==false){
-        champ_predvide.textContent ="";
-        
-        //alert("passe meme ici");
+    if (content_missing_box_align()){
+        return false;
     }
-    
-   // alert("passe dans fonc format");
-   //alert("format align retourne");
-    var align = document.getElementById("align").value;
+   
     var textArea = document.getElementById("align");
     var arrayOfLines = textArea.value.split("\n"); 
+    var nolignes = arrayOfLines.length; //le nombre de lignes total 
     
- 
+    var i;
     
+    //Lecture des lignes vides /entres 
+    //On commence a i=1 car la premiere ligne contient le titre
+    for(i=1; i<arrayOfLines.length; i++){
+        if(arrayOfLines[i].length !== 0){
+               break;
+        }
+    }
+   
+    var j=0; //commencer a partir de la ligne contenant le sequence
+    var premiere_i = i;
+     
+    while(j<5){
+               
+        //Arrive a fin      
+        if (i+j > nolignes-1){
+             break;
+        }
+        
+        //Lecture d'une ligne
+        var line = arrayOfLines[i+j]; 
+        var longligne = line.length;
+        
+        if (j===0){
+            var longfirsli = longligne;
+        }
+        /*
+         //test si longeur d'une ligne est bien 73
+        if(longligne !== 73){
+           champ_alignvide.textContent = "Incorrect Format";
+           champ_alignvide.style.color = "red";
+           return true;
+        }
+        */
+        //test si la premiere colonne ne contient pas d'espace
+        var col1 = line.substring(0,7);
+        if (col1.includes(" ")){
+             champ_alignvide.textContent = "Incorrect Format";
+             champ_alignvide.style.color = "red";
+             return true;
+        
+        //Sinon on le stock dans notre var global pour verifier la coherence 
+        }else{
+            if(i  === premiere_i ){
+            arraySeq.push(col1);
+           
+            }else{
+                
+                if(col1 !== arraySeq[j]){
+                    alert("oops ne correspondent pas");
+                    champ_alignvide.textContent = "Incorrect Format";
+                    champ_alignvide.style.color = "red";
+                     return true;
+                    
+                }
+            }
+        }
+        
+        //test 2e colonne est bien 'vide'
+        var col2 = line.substring(7,13);
+        if (col2 !== "      "){
+             champ_alignvide.textContent = "Incorrect Format";
+             champ_alignvide.style.color = "red";
+             return true;
+        }
+        
+        //test si les longueurs des 3e colonnes d'un meme groupe sont egales       
+        var longcol3 = longligne - 13 + 1;
+        if (longcol3 !== longfirsli -13 + 1 ){
+             alert("difference longueur!!!");
+             champ_alignvide.textContent = "Incorrect Format";
+             champ_alignvide.style.color = "red";
+             return true;
+        }
+        //test si la 3e colonne ne contient pas d'espace
+        var col3 = line.substring(13,longfirsli);
+        if (col3.includes(" ")){
+            champ_alignvide.textContent = "Incorrect Format";
+            champ_alignvide.style.color = "red";
+            return true;
+        }
+        
+        //Arrive a fin d'un groupe
+        if(j===4){
+            i = i + 7; //Passer au groupe suivant ignorant la ligne contenant les : . *
+            j=0;
+        }else{
+            j++;
+        }
+    }
+       
+    return false; 
+}
+
+// TO DO TO DO ! INCOMPLETE 
+// Fonction testant si le format dans boite 2 incorrecte   -> TO DO 
+function format_incorrect_box_pred(){
+   if (content_missing_box_pred()){
+       return false;
+   }
+   
+    var textArea = document.getElementById("pred");
+    var arrayOfLines = textArea.value.split("\n"); 
     var i;
     for(i=0; i<arrayOfLines.length; i++){
      //   alert("test length");
        // alert((arrayOfLines[i].length != 3));
         if(arrayOfLines[i].length !== 3){
-           //  alert("rentre dans if");
-            var champ_alignvide = document.getElementById("alignvide");
-             champ_alignvide.textContent = "Incorrect Format";
-             champ_alignvide.style.color = "red";
+     
+             champ_predvide.textContent = "Incorrect Format";
+             champ_predvide.style.color = "red";
              //alert("true");
             return true;
         }
     }
     
-    
-  /*  var bool = false;
-    var align = document.getElementById("align").value;
-    if (align !="a"){
-         var champ_alignvide = document.getElementById("alignvide");
-                   champ_alignvide.textContent = "Incorrect Format";
-                   champ_alignvide.style.color = "red";
-                    bool = true;
-        
-    }
-    */
-    //alert("false");
-    return false;
-    
+    return false; 
 }
+
+
+
+
 
 
 
