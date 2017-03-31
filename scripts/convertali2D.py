c2892ec962c213febf3a54aded1f8217d11c4ba2
Priya <3408247@github.com>
2017-03-31 22:11:56 +0200

    debug

--- scripts/convertali2D.py
+++ scripts/convertali2D.py
@@ -6,6 +6,13 @@ import sys
 
 ######################################################################################################################################
 def convertFile(fileInput, fileOutput):
+	"""Function that converts ali2D.input file to ali2D.output format 
+	   Prints error message if input file not found
+	   
+	   Args:
+		fileInput (filepath) : the file in ali2D.input format to be converted
+		fileOutput(filepath) : the output file
+	"""
 
 	try:
 		f = open(fileInput, "r")
@@ -20,33 +27,45 @@ def convertFile(fileInput, fileOutput):
 	g = open(fileOutput, "w")
 	
 	for i in range (0, len(lines)):
-		if i % 6 == 0:
-			words = lines[i].split(":")   	# ['  name0', ' d1or4a_\n']
-			seq1 = words[1].split("\n")	# [' d1itha_', '']
-			seq = seq1[0].split(" ")[1]	# 'd1itha_'
-			g.write(seq)
+		
+		if lines[i]!="\n" and lines[i]!="":             # because file may contain blank lines at the end
 
-		if i % 6 == 1:
-			words = lines[i].split(":")	# ['  seq0', ' -------------------ETAYFSDSNGQQKNRI- ... \n']
-			seq2 = words[1].split("\n")	# [' -------------------ETAYFSDSNGQQKNRI- ...', '']
-			seq = seq2[0]			# '-------------------ETAYFSDSNGQQKNRI- ...'
+			if i % 6 == 0:
+				words = lines[i].split(":")   	# ['  name0', ' d1or4a_\n']
+				seq1 = words[1].split("\n")	# [' d1itha_', '']
+				seq = seq1[0].split(" ")[1]	# 'd1itha_'
+				g.write(seq)
+
+			if i % 6 == 1:
+				words = lines[i].split(":")	# ['  seq0', ' -------------------ETAYFSDSNGQQKNRI- ... \n']
+				seq2 = words[1].split("\n")	# [' -------------------ETAYFSDSNGQQKNRI- ...', '']
+				seq = seq2[0]			# '-------------------ETAYFSDSNGQQKNRI- ...'
 			
-			g.write(seq)
+				g.write(seq)
 
-		if i % 6 == 3:
-			words = lines[i].split(":")	# ['seq_struct0',' -------------------CCCCCCCCCCCCCCCC-------CCC---CCH..\n']
-			seq1 = words[1].split(" ")	# ['','-------------------CCCCCCCCCCCCCCCC-------CCC---CCH..\n']
-			seq = seq1[1].split("\n")[0]	# '-------------------CCCCCCCCCCCCCCCC-------CCC---CCH..'
+			if i % 6 == 3:
+				words = lines[i].split(":")	# ['seq_struct0',' -------------------CCCCCCCCCCCCCCCC-------CCC---CCH..\n']
+				seq1 = words[1].split(" ")	# ['','-------------------CCCCCCCCCCCCCCCC-------CCC---CCH..\n']
+				seq = seq1[1].split("\n")[0]	# '-------------------CCCCCCCCCCCCCCCC-------CCC---CCH..'
 
-			g.write("\t")
-			g.write(seq)
-			g.write("\n")
+				g.write("\t")
+				g.write(seq)
+				g.write("\n")
 	g.close()
 
 
 
 ######################################################################################################################################
 def readArgs(args):
+	"""Function that reads the arguments in command line to execute the covertali2D.py script correctly
+	   The correct format is : 'python convertali2D.py <inputfile> <outputfile>'
+	   Prints 'DONE!' if script executed correctly
+	   Prints appropriate Error Message otherwise
+
+	   Args:
+		args (str) : the string input on the command line
+	"""
+
 
 	if len(args)!= 3 :
 		print "ERROR:: Parameters ali2D inputFile and outputFile required\n"
